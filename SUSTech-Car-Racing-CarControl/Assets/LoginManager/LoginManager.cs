9167c9ebca7259cc14d1238082bcce88519de95a
Shenwenkun <12211717@mail.sustech.edu.cn>
2025-04-11 17:27:29 +0800

    UI_design document

--- SUSTech-Car-Racing-CarControl/Assets/LoginManager/LoginManager.cs
+++ SUSTech-Car-Racing-CarControl/Assets/LoginManager/LoginManager.cs
@@ -30,7 +30,13 @@ public class LoginManager : MonoBehaviour
         string password = passwordInput.text;
 
         // 简单的登录验证
-        if (id == "12211717" && password == "Sh040416")
+        /*
+         * 登录不成功报提示信息、
+         * 登录成功先进入选择游戏模式菜单（隐藏login层元素，显示menu层元素）
+         * menu层包括（两个按钮【单人模式、多人模式】、右上角设置按钮、排行榜按钮）
+         * 单人模式按照该代码切换摄像机、多人模式则按钮下飞出两个小按钮（创建房间、加入房间）
+         */
+        if (id == "admin" && password == "123456")
         {
             // 登录成功，调用摄像机切换器切换摄像机
             cameraSwitcher.OnLoginSuccess();
