26e6364f103aa46d4fc15fe2245562c4855a0100
wixun <1415768176@qq.com>
2025-05-10 14:06:02 +0800

    add gamemenu UI

---
+++ Assets/Scripts/Entity/Settings.cs
@@ -0,0 +1,8 @@
+namespace Entity
+{
+    public class Settings
+    {
+    
+    }
+}
+
---
+++ Assets/Scripts/Manager/SaveManager.cs
@@ -0,0 +1,12 @@
+using Entity;
+
+namespace Manager
+{
+    public class SaveManager : Singleton<SaveManager>
+    {
+        public void SaveSetting(Settings settings)
+        {
+            
+        }
+    }
+}
