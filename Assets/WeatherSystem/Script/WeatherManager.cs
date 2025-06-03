3cc2b80eb67de5b0d95454a0aac78aa6a8d3eb7a
WDongZ <12210833@mail.sustech.edu.cn>
2025-06-03 16:27:05 +0800

    Weather System

---
+++ Assets/WeatherSystem/Script/WeatherManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using System.Collections;
+
+public enum WeatherType {
+    Sunny,
+    Rainy
+}
+
+public class WeatherManager : MonoBehaviour {
+    public static WeatherManager Instance;
+
+    [Header("Weather Volume Profiles")]
+    public VolumeProfile sunnyProfile;
+    public VolumeProfile rainyProfile;
+
+    [Header("Particle Effects")]
+    public GameObject rainParticle;
+
+    [Header("Audio")]
+    public AudioSource ambientAudioSource;
+    public AudioClip rainSound;
+
+    private Volume volume;
+    private WeatherType currentWeather = WeatherType.Sunny;
+
+    void Awake() {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        volume = FindObjectOfType<Volume>();
+        rainParticle.SetActive(false);
+    }
+
+    public void SetWeather() {
+        StopAllCoroutines();
+        if (currentWeather == WeatherType.Sunny) {
+            StartCoroutine(TransitionWeather(WeatherType.Rainy));
+        } else {
+            StartCoroutine(TransitionWeather(WeatherType.Sunny));
+        }
+        
+    }
+
+    IEnumerator TransitionWeather(WeatherType newWeather) {
+
+        switch (newWeather) {
+            case WeatherType.Sunny:
+                rainParticle.SetActive(false);
+                volume.profile = sunnyProfile;
+                break;
+            case WeatherType.Rainy:
+                volume.profile = rainyProfile;
+                rainParticle.SetActive(true);
+                ambientAudioSource.clip = rainSound;
+                break;
+        }
+
+        currentWeather = newWeather;
+        ambientAudioSource.Play();
+
+        yield return null;
+    }
+}
